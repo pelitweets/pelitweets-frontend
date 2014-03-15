84e3a184d10be6022b8c2e355b15648e5900e35c
carlosazaustre <cazaustre@gmail.com>
2014-03-15 15:37:17 +0100

    change api url

--- app/scripts/controllers/moviedetail.js
+++ app/scripts/controllers/moviedetail.js
@@ -2,7 +2,7 @@
 
 angular.module('pelitweets').controller('MovieDetailController',
 	function MovieDetailController($scope, $http, $routeParams, $location) {
-		$http.get('/api/movie/'+ $routeParams.movieId).
+		$http.get('http://pelitweets-dev.herokuapp.com/api/movie/'+ $routeParams.movieId).
 			success(function(data, status, headers, config) {
 				if(data) {
 					$scope.movie = data;
--- app/scripts/controllers/movielist.js
+++ app/scripts/controllers/movielist.js
@@ -2,7 +2,7 @@
 
 angular.module('pelitweets').controller('MovieListController',
 	function MovieListController($scope, $http, $routeParams, $location) {
-		$http.get('/api/movies').
+		$http.get('http://pelitweets-dev.herokuapp.com/api/movies').
 			success(function(data, status, headers, config) {
 				if(data) {
 					$scope.movies = data;
