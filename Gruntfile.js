8f6e69e8a6f1b371e0082a77f00a194b65b891f8
carlosazaustre <cazaustre@gmail.com>
2014-03-16 20:28:14 +0100

    added Google Analytics code

--- Gruntfile.js
+++ Gruntfile.js
@@ -262,6 +262,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
+            'CNAME',
             '*.html',
             'views/{,*/}*.html',
             'bower_components/**/*',
